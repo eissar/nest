bdcdadf0d5587d617386a966afa5709c55292069
Eshaan <eshaanissar@outlook.com>
2025-02-21 00:00:33 -0500

    working on eagleapi...

--- eagle/api/api.go
+++ eagle/api/api.go
@@ -3,7 +3,6 @@ package api
 // wrapper for every path in the eagle library.
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -11,18 +10,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 )
-
-type Item struct {
-	URL              string            `json:"url"`
-	Name             string            `json:"name"`
-	Website          string            `json:"website,omitempty"`
-	Tags             []string          `json:"tags,omitempty"`
-	Star             int               `json:"star,omitempty"`
-	Annotation       string            `json:"annotation,omitempty"`
-	ModificationTime int64             `json:"modificationTime,omitempty"`
-	FolderID         string            `json:"folderId,omitempty"`
-	Headers          map[string]string `json:"headers,omitempty"`
-}
 
 type Endpoint struct {
 	Path    string
@@ -34,6 +21,11 @@ type EagleApiErr struct {
 	Message  string
 	Endpoint Endpoint
 	Err      error
+}
+
+type EagleResponse struct {
+	Status string
+	Data   []Item
 }
 
 type ApiKeyErr struct {
@@ -58,103 +50,93 @@ func getApiKey() (string, error) {
 	return accessToken, nil
 }
 
-func InvokeRaindropAPI(endpoint Endpoint, body interface{}) (string, error) {
+// adds api key to request and
+// returns *EagleResponse.
+//
+// TODO: we check if status == success anyways, so
+// can we just return EagleResponse.Data?
+func InvokeRaindropAPI(req *http.Request, body interface{}) (*EagleResponse, error) {
+	var result EagleResponse
 	key, err := getApiKey()
 	if err != nil {
-		return "", err
-	}
-	fmt.Println(key)
-	return "a", nil
-
-	/*
-		headers := map[string]string{
-			"Authorization": "Bearer " + key,
-			"Content-Type":  "application/json",
-		}
-
-		var requestBody []byte
-		if body != nil {
-			if bodyStr, ok := body.(string); ok {
-				requestBody = []byte(bodyStr)
-			} else {
-				jsonBody, err := json.Marshal(body)
-				if err != nil {
-					return "", fmt.Errorf("error marshaling body to JSON: %v", err)
-				}
-				requestBody = jsonBody
-			}
-		}
-
-		req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(requestBody))
-		if err != nil {
-			return "", fmt.Errorf("error creating request: %v", err)
-		}
-
-		for k, v := range headers {
-			req.Header.Set(k, v)
-		}
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return "", fmt.Errorf("error making request: %v", err)
-		}
-		defer resp.Body.Close()
-
-		responseBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return "", fmt.Errorf("error reading response body: %v", err)
-		}
-
-		if resp.StatusCode != http.StatusOK {
-			return string(responseBody), fmt.Errorf("API Error: Status Code %d, Response: %s", resp.StatusCode, string(responseBody))
-		}
-
-		return string(responseBody), nil
-	*/
-}
-
-// docs: https://api.eagle.cool/item/add-from-url
-func AddItemFromURL(baseURL string, item Item) (map[string]interface{}, error) {
-	itemJSON, err := json.Marshal(item)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling item to JSON: %v", err)
+		return &result, err
 	}
 
-	req, err := http.NewRequest("POST", baseURL+"/api/item/addFromURL", bytes.NewBuffer(itemJSON))
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %v", err)
-	}
+	query := req.URL.Query()
+	query.Add("key", key)
 
-	req.Header.Set("Content-Type", "application/json")
-
-	if item.Headers != nil {
-		for k, v := range item.Headers {
-			req.Header.Set(k, v)
-		}
-	}
+	req.URL.RawQuery = query.Encode()
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error making request: %v", err)
+		return &result, fmt.Errorf("error making request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	var result map[string]interface{}
 	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
-		return nil, fmt.Errorf("error decoding response: %v", err)
+		return &result, fmt.Errorf("error decoding response: %v", err)
 	}
 
-	if resp.StatusCode != http.StatusOK { // Check for non-200 status codes
-		return nil, fmt.Errorf("server returned non-200 status: %d, Response: %v", resp.StatusCode, result)
+	// TODO: do all responses have a status key?
+	if result.Status != "success" {
+		return &result, fmt.Errorf("error decoding response: result object's response was not `success`, but instead, %s ", result.Status)
 	}
 
-	return result, nil
+	return &result, nil
 }
+
+// docs: https://api.eagle.cool/item/add-from-url
 
 func RegisterGroupRoutes(g *echo.Group) {
 	g.GET("/item/addFromURL", handleAddItemFromUrl)
 
 }
+
+/*
+	headers := map[string]string{
+		"Authorization": "Bearer " + key,
+		"Content-Type":  "application/json",
+	}
+
+	var requestBody []byte
+	if body != nil {
+		if bodyStr, ok := body.(string); ok {
+			requestBody = []byte(bodyStr)
+		} else {
+			jsonBody, err := json.Marshal(body)
+			if err != nil {
+				return "", fmt.Errorf("error marshaling body to JSON: %v", err)
+			}
+			requestBody = jsonBody
+		}
+	}
+
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %v", err)
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return string(responseBody), fmt.Errorf("API Error: Status Code %d, Response: %s", resp.StatusCode, string(responseBody))
+	}
+
+	return string(responseBody), nil
+*/
