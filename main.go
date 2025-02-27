da298b10126bc4d8152bc6f33cc1d626e72ebf12
eissar <61753097+eissar@users.noreply.github.com>
2025-02-26 22:10:37 -0500

    initial logic - make run-server a flag, so we can use nest as cli.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+
 	"github.com/eissar/nest/core"
 )
 
@@ -9,21 +10,26 @@ import (
 var debug = false
 
 func main() {
-	//#region parseFlags
-	d := flag.Bool("debug", true, "shows additional information in the console while running. (does nothing)")
+
+	help := flag.Bool("help", false, "print help information")
+	serve := flag.Bool("serve", false, "run the utility server")
+	//debug := flag.Bool("debug", true, "shows additional information in the console while running. (does nothing)")
 	flag.Parse()
-	debug = *d
-	//#endregion
-	if debug {
-		/* pwsh.ExecPwshCmd("./powershell-utils/openUrl.ps1 -Uri 'http://localhost:1323/app/notes'") */
+
+	if *help || flag.NFlag() == 0 {
+		flag.PrintDefaults()
 	}
 
-	// trySearch()
-	core.Start() //blocking
+	//if *debug { } /* pwsh.ExecPwshCmd("./powershell-utils/openUrl.ps1 -Uri 'http://localhost:1323/app/notes'") */
 
-	// TODO:?
-	// replace runServer()
-	// with:
-	// server = echo.New()
-	// core.RegisterRoutes(server)
+	if *serve {
+		core.Start() //blocking
+	}
+
 }
+
+// TODO:?
+// replace runServer()
+// with:
+// server = echo.New()
+// core.RegisterRoutes(server)
