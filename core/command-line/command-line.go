6e3b3f8835acb76d3484e70397cc903f60dcca37
Eshaan Issar <eshaanissar@outlook.com>
2025-09-07 17:37:19 -0400

    refactor: move formatters into different file

--- core/command-line/command-line.go
+++ core/command-line/command-line.go
@@ -13,7 +13,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/go-logfmt/logfmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 
@@ -265,46 +264,6 @@ func RecentLibraries() *cobra.Command {
 	}
 
 	return librariesCmd
-}
-
-func logFmtStdOut(data []*api.ListItem, props []string) {
-	outp := logfmt.NewEncoder(os.Stdout)
-
-	outp.EncodeKeyvals("test", nil)
-	outp.EndRecord()
-	// for _, item := range data {
-	//	// outp.EncodeKeyval("url", item.URL)
-	//	// outp.EncodeKeyval("folderIds", strings.Join(item.Folders, ", "))
-	//	outp.EndRecord()
-	// }
-
-}
-
-// exclude properties to exclude
-func jsonFmtStdOut(cmd *cobra.Command, data []*api.ListItem, exclude []string) error {
-	// json.NewEncoder(os.Stdout)
-
-	b, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	var m []map[string]interface{}
-	if err := json.Unmarshal(b, &m); err != nil {
-		return err
-	}
-
-	for i, _ := range m {
-		for _, key := range exclude {
-			delete(m[i], key)
-		}
-	}
-
-	st := json.NewEncoder(os.Stdout)
-
-	st.Encode(m)
-
-	return nil
 }
 
 func rebuildCmdText(cmd *cobra.Command, args []string) string {
